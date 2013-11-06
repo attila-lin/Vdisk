14ce69bee975d8ba7de9c718c91f67d2d03899ae
whatever1992 <799758730@qq.com>
2013-11-06 15:43:30 +0800

    add server

---
+++ README.md
@@ -0,0 +1,4 @@
+VDISK
+====
+
+A Dropbox like application using [Vdike(微盘)](http://vdisk.weibo.com/)
--- src/test.py
+++ src/test.py
@@ -11,26 +11,27 @@ import urllib2
 
 from vdisksdk import *
 
-oauth2 = OAuth2('1734683723', '9a60088a5b1d27e261ef532c551a3298', 'http://10.214.9.220/')
+oauth2 = OAuth2('1734683723', '9a60088a5b1d27e261ef532c551a3298', 'http://localhost/vdisk/getcode.php')
 
-# print oauth2.authorize()
+response = urllib2.urlopen('http://localhost/vdisk/getlastid.php')
+myid = response.read()
+print myid
 
-# code = '729d580421625fca46273609aca3d582'
+# gain weburl
+
+print oauth2.authorize(state = myid)
+
+# code = '5dea2d25032d83522c0f6a7d56563907'
 # print oauth2.access_token(code = code)
 
-string = '{"access_token":"6df7806662f5c9H1ToytJ3KZkzH80b52","expires_in":1383674205,"time_left":85780,"uid":"2055149073","refresh_token":"75e36b6662f5c9H1ToytJ3KZkzH6bf0e"}'
-acc = eval(string)
-print acc['access_token']
+# string = '{"access_token":"938aeb6662f5c9H1ToytJ3KZkzHeb753","expires_in":1383806180,"time_left":86400,"uid":"2055149073","refresh_token":"75e36b6662f5c9H1ToytJ3KZkzH6bf0e"}'
 
-# {
-# 	"access_token":"6df7806662f5c9H1ToytJ3KZkzH80b52",
-# 	"expires_in":1383674205,
-# 	"time_left":85780,
-# 	"uid":"2055149073",
-# 	"refresh_token":"75e36b6662f5c9H1ToytJ3KZkzH6bf0e"
-# }
+# acc = eval(string)
+# print acc['access_token']
 
-client = Client()
-info = client.account_info(acc['access_token'])
+
+# client = Client()
+# info = client.account_info(acc['access_token'])
+# print info
 
 # {"uid":"1764170","sina_uid":"2055149073","quota_info":{"quota":"96292831232","consumed":"2090080985"},"verified":false,"screen_name":"\u660a\u58a8\u662f\u4e2a\u51b7\u573a\u5e1d","user_name":"\u660a\u58a8\u662f\u4e2a\u51b7\u573a\u5e1d","location":"\u6d59\u6c5f \u5b81\u6ce2","profile_image_url":"http:\/\/tp2.sinaimg.cn\/2055149073\/50\/40031506387\/1","avatar_large":"http:\/\/tp2.sinaimg.cn\/2055149073\/180\/40031506387\/1","gender":"m"}
