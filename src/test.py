727b0f62a597b353c139e96c2d8b54fe9ad3797b
whatever1992 <799758730@qq.com>
2013-11-06 21:52:21 +0800

    server done

--- src/test.py
+++ src/test.py
@@ -2,13 +2,8 @@
 # encoding: utf-8
 
 import sys
-import logging
 import time
-import mimetypes
-import urllib
-import urllib2
 import os
-import sys
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -28,7 +23,7 @@ print myid
 
 
 # 2. get authenticate web url
-myid = '1'
+# myid = '1'
 oauth2 = OAuth2(app_key, app_secret, call_back_url)
 #post id
 weburl = oauth2.authorize(state = myid)
@@ -37,6 +32,7 @@ weburl = oauth2.authorize(state = myid)
 # 3. open a browser with the authenticate web
 import webbrowser
 webbrowser.open_new_tab(weburl)
+
 
 # 4. waiting and ask whether you accept it with a dialog
 # In the servers first I get the code with call_back_url.
@@ -64,21 +60,38 @@ app.MainLoop()
 
 # 5. if accepted, get code with post myid to 'getcode.php'
 # else ...
-code = '5dea2d25032d83522c0f6a7d56563907'
+
+
+#TODO
+import urllib2, urllib
+# myid = 4
+data = {
+        'id' : myid,
+        }
+f = urllib2.urlopen(
+        url     = 'http://localhost/vdisk/getcode.php',
+        data    = urllib.urlencode(data)
+    )
+code = f.read()
+print code
+
 
 
 # 6. After getting code we can get access_token
 access_token_str =  oauth2.access_token(code = code)
 access_token_dir = eval(access_token_str)
-# string = '{"access_token":"938aeb6662f5c9H1ToytJ3KZkzHeb753","expires_in":1383806180,"time_left":86400,"uid":"2055149073","refresh_token":"75e36b6662f5c9H1ToytJ3KZkzH6bf0e"}'
 
-acc_access_val = acc['access_token']
-acc_expire_val = acc['expires_in']
-acc_uid_val = acc['uid']
-acc_refresh_token = acc['refresh_token']
+print access_token_dir
+
+acc_access_val = access_token_dir['access_token']
+acc_expire_val = access_token_dir['expires_in']
+acc_uid_val    = access_token_dir['uid']
+acc_refresh_token = access_token_dir['refresh_token']
+
+print acc_expire_val
 
 # 7. use the sdk 
 client = Client()
-info = client.account_info(acc['access_token'])
-print info
+info = client.account_info(access_token_dir['access_token'])
 
+print info
